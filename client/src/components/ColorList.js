8898516573dc21c4b4c1b3ae72fa8d103f169c7f
Harry Merritt <harry@merritt.es>
2020-03-22 23:35:26 +0000

    Completed the saveEdit function

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,89 +1,112 @@
 import React, { useState } from "react";
-import axios from "axios";
+import {axiosWithAuth} from "../utils/axiosWithAuth";
+import { sortBy } from "lodash";
 
 const initialColor = {
-  color: "",
-  code: { hex: "" }
+    color: "",
+    code: { hex: "" }
 };
 
 const ColorList = ({ colors, updateColors }) => {
+    const [editing, setEditing] = useState(false);
+    const [colorToEdit, setColorToEdit] = useState(initialColor);
 
-  const [editing, setEditing] = useState(false);
-  const [colorToEdit, setColorToEdit] = useState(initialColor);
+    const editColor = color => {
+        setEditing(true);
+        setColorToEdit(color);
+    };
 
-  const editColor = color => {
-    setEditing(true);
-    setColorToEdit(color);
-  };
+    const saveEdit = e => {
+        e.preventDefault();
 
-  const saveEdit = e => {
-    e.preventDefault();
-    // Make a put request to save your updated color
-    // think about where will you get the id from...
-    // where is is saved right now?
-  };
+        axiosWithAuth()
+            .put(`/colors/${colorToEdit.id}`, colorToEdit)
+            .then(res => {
+                console.log("[ColorList] Updated color:", res.data);
 
-  const deleteColor = color => {
-    // make a delete request to delete this color
-  };
+                updateColors(
+                    sortBy(
+                        [
+                            ...colors.filter(color => color.id !== res.data.id),
+                            res.data
+                        ],
+                        ["id"]
+                    )
+                );
+            })
+            .catch(err => {
+                console.log("[ColorList] Error updating color:", err);
+            })
+    };
 
-  return (
-    <div className="colors-wrap">
-      <p>colors</p>
-      <ul>
-        {colors.map(color => (
-          <li key={color.color} onClick={() => editColor(color)}>
-            <span>
-              <span className="delete" onClick={e => {
-                    e.stopPropagation();
-                    deleteColor(color)
-                  }
-                }>
-                  x
-              </span>{" "}
-              {color.color}
-            </span>
-            <div
-              className="color-box"
-              style={{ backgroundColor: color.code.hex }}
-            />
-          </li>
-        ))}
-      </ul>
-      {editing && (
-        <form onSubmit={saveEdit}>
-          <legend>edit color</legend>
-          <label>
-            color name:
-            <input
-              onChange={e =>
-                setColorToEdit({ ...colorToEdit, color: e.target.value })
-              }
-              value={colorToEdit.color}
-            />
-          </label>
-          <label>
-            hex code:
-            <input
-              onChange={e =>
-                setColorToEdit({
-                  ...colorToEdit,
-                  code: { hex: e.target.value }
-                })
-              }
-              value={colorToEdit.code.hex}
-            />
-          </label>
-          <div className="button-row">
-            <button type="submit">save</button>
-            <button onClick={() => setEditing(false)}>cancel</button>
-          </div>
-        </form>
-      )}
-      <div className="spacer" />
-      {/* stretch - build another form here to add a color */}
-    </div>
-  );
+    const deleteColor = color => {
+        // make a delete request to delete this color
+    };
+
+    return (
+        <div className="colors-wrap">
+            <p>colors</p>
+            <ul>
+                {colors.map(color => (
+                    <li key={color.color} onClick={() => editColor(color)}>
+                        <span>
+                            <span
+                                className="delete"
+                                onClick={e => {
+                                    e.stopPropagation();
+                                    deleteColor(color);
+                                }}
+                            >
+                                x
+                            </span>{" "}
+                            {color.color}
+                        </span>
+                        <div
+                            className="color-box"
+                            style={{ backgroundColor: color.code.hex }}
+                        />
+                    </li>
+                ))}
+            </ul>
+            {editing && (
+                <form onSubmit={saveEdit}>
+                    <legend>edit color</legend>
+                    <label>
+                        color name:
+                        <input
+                            onChange={e =>
+                                setColorToEdit({
+                                    ...colorToEdit,
+                                    color: e.target.value
+                                })
+                            }
+                            value={colorToEdit.color}
+                        />
+                    </label>
+                    <label>
+                        hex code:
+                        <input
+                            onChange={e =>
+                                setColorToEdit({
+                                    ...colorToEdit,
+                                    code: { hex: e.target.value }
+                                })
+                            }
+                            value={colorToEdit.code.hex}
+                        />
+                    </label>
+                    <div className="button-row">
+                        <button type="submit">save</button>
+                        <button onClick={() => setEditing(false)}>
+                            cancel
+                        </button>
+                    </div>
+                </form>
+            )}
+            <div className="spacer" />
+            {/* stretch - build another form here to add a color */}
+        </div>
+    );
 };
 
 export default ColorList;
